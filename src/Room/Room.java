e797113050d32d68eb519f670696b65170657c42
Lipcsey Márton <lipcsey.magyar.marton@gmail.com>
2024-03-20 15:41:42 +0100

    Added Room Class

---
+++ src/Room/Room.java
@@ -0,0 +1,11 @@
+package Room;
+import Item.*;
+import Player.*;
+import java.util.*;
+
+public abstract class Room {
+    public int capacity;
+    public List<Item> items;
+    public List<Player> players;
+
+}
