266626d737405e59e49e5d1c30c8747a5a9bdf43
Kiss Hunor <kisshunor03@gmail.com>
2024-04-06 19:54:31 +0200

    FakeTvsz-bol kivettem a felesleges dolgokat

--- src/Item/FakeTvsz.java
+++ src/Item/FakeTvsz.java
@@ -6,25 +6,10 @@ public class FakeTvsz extends Item {
 
     public FakeTvsz(){}
 
-    // These functions are implemented in Item
-
-    @Override
-    public boolean NeedToThrow() {
-        Logger.logEntry(this.getClass().getName(), "NeedToThrow", "");
-        Logger.logExit(this.getClass().getName(), "NeedToThrow", "false");
-        return false;
-    }
-
     @Override
     public void Use(Player player) {
         Logger.logEntry(this.getClass().getName(), "Use", "player");
         Logger.logExit(this.getClass().getName(), "Use");
     }
 
-    @Override
-    public boolean CanSave(Player player){
-        Logger.logEntry(this.getClass().getName(), "CanSave", "player");
-        Logger.logExit(this.getClass().getName(), "CanSave", "false");
-        return false;
-    }
 }
